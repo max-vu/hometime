89fe45583d9814b99ac1eb5475cd62e88351bd51
Max Vu <max.vu.goldenowl@gmail.com>
2021-11-04 17:07:37 +0700

    remove sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2021_11_03_152730) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "guests", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
     t.string "last_name"
-    t.string "phone_numbers"
+    t.string "phone_numbers", array: true
     t.text "localized_description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
