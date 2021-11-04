4f4af59c16ec3d97746563afe0562ef86205eea7
Max Vu <max.vu.goldenowl@gmail.com>
2021-11-04 15:30:51 +0700

    fixed rspec

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,10 +4,10 @@ class Reservation < ApplicationRecord
   validates :code, presence: true, uniqueness: true
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates :nights, presence: true, length: { minimum: 1 }
-  validates :guests, presence: true, length: { minimum: 1 }
-  validates :adults, presence: true, length: { minimum: 1 }
+  validates :nights, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :guests, presence: true, numericality: { greater_than_or_equal_to: 1 }
+  validates :adults, presence: true, numericality: { greater_than_or_equal_to: 1 }
 
-  validates_datetime :start_at, after: :now, after_message: 'Must not be in the past'
+  validates_datetime :start_date, after: :now, after_message: 'Must not be in the past'
   validates_datetime :end_date, after: :start_date, after_message: 'must be after start at date'
 end
